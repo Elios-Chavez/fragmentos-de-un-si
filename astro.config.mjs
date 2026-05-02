c5a228a1aade9945617e309997c4e2aa1f241b0b
ELIAS RAMONN CHAVEZ SILVA <eliaschavez345645@gmail.com>
2026-05-01 21:11:28 -0600

    chore: trigger github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://elios-chavez.github.io/fragmentos-de-un-si',
+  site: 'https://Elios-Chavez.github.io/fragmentos-de-un-si',
   base: '/fragmentos-de-un-si/'
 });
